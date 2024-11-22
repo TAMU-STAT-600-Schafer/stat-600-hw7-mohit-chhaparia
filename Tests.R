8cd62d61aa13ac2ef236d2866561c752e61a6a59
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-22 16:05:24 -0600

    Added a test on loss_grad_scores function to verify output dimensions.

--- Tests.R
+++ Tests.R
@@ -3,6 +3,10 @@ source("FunctionsNN.R")
 
 library(testthat)
 
+##########################
+# Tests on initialize_bw #
+##########################
+
 test_that("initialize_bw works correctly", {
   expect_error(initialize_bw(p = 0, hidden_p = 2, K = 2))
   expect_error(initialize_bw(p = 3, hidden_p = 0, K = 2))
@@ -22,3 +26,20 @@ test_that("initialize_bw works correctly with default and custom scale values", 
   expect_true(abs(W1_sd - 0.05) < 5e-2)
   expect_true(abs(W2_sd - 0.05) < 5e-2)
 })
+
+#############################
+# Tests on loss_grad_scores #
+#############################
+
+test_that("loss_grad_scores works correctly", {
+  scores <- matrix(1:3, nrow = 1)
+  out <- loss_grad_scores(y = c(2), scores = scores, K = 3)
+  expect_equal(dim(out$grad), dim(scores))
+  expect_equal(out$error, 0)
+  
+  scores <- matrix(c(0.1, 0.5, 0.4, 0.2, 0.3, 0.5),
+                   nrow = 2, byrow = TRUE)
+  out <- loss_grad_scores(y = c(0, 2), scores = scores, K = 3)
+  expect_equal(dim(out$grad), dim(scores))
+})
+
