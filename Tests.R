7e3ae00458fe32ae3ccbfa9b0714575e6b3ecb01
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-22 15:49:31 -0600

    Added tests on initialize_bw function with incorrect inputs.

--- Tests.R
+++ Tests.R
@@ -2,3 +2,11 @@
 source("FunctionsNN.R")
 
 library(testthat)
+
+test_that("initialize_bw works correctly", {
+  
+  expect_error(initialize_bw(p = 0, hidden_p = 2, K = 2))
+  expect_error(initialize_bw(p = 3, hidden_p = 0, K = 2))
+  expect_error(initialize_bw(p = 3, hidden_p = 2, K = 0))
+  
+})
